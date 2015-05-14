e85678372e97a039d806a26e6b5ed8656d11d96a
tomsmeding <hallo@tomsmeding.nl>
2015-05-14 19:43:35 +0200

    Add string manipulation functions to util.h

--- util.h
+++ util.h
@@ -1,6 +1,29 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
 int genid(void) {
 	static id = 0;
 	return id++;
+}
+
+vector<string> split(const string &s,const char delim){
+	string::size_type cursor=0;
+	string::size_type idx=s.find(delim);
+	vector<string> vec;
+	while(idx!=string::npos){
+		vec.push_back(s.substr(cursor,idx-cursor));
+		cursor=idx+1;
+		idx=s.find(delim,cursor);
+	}
+	return vec;
+}
+
+string trim(const string &s){
+	string::size_type begin=s.find_first_not_of(" \t"),
+	                  end=s.find_last_not_of(" \t");
+	return s.substr(begin,end-begin);
 }
